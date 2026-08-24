3286a68ad9e9fb169ea527a1a55261766443eb37
Aymara Marin <aymaramarincorrales@gmail.com>
2026-08-24 12:58:46 -0600

    Update bme280.ino
    Librerias

--- firmware/bme280/bme280.ino
+++ firmware/bme280/bme280.ino
@@ -1,4 +1,11 @@
-// aca va el codigo principal
+// codigo principal
 
 // Channel ID: 3464038
 //write api key:I94PJNZYX31GQFPE 
+
+
+#include <WiFi.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <ThingSpeak.h>
